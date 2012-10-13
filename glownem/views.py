ef61182d2d1e7640c3b3766bb06519bdb9b8522a
Austin Whittier <austin.whitt@gmail.com>
2012-10-13 02:22:37 -0700

    Working on views

--- glownem/views.py
+++ glownem/views.py
@@ -1,13 +1,13 @@
 from django.shortcuts import render_to_response
-from movies.helpers import nearby_locations
+# from movies.helpers import nearby_locations
 
 def home(request):
 	return render_to_response('home.html')
 
-def get(request):
-	sw_latitude = request.GET.get('slat', '')
-	sw_longitude = request.GET.get('slon', '')
-	ne_latitude = request.GET.get('nlat', '')
-	ne_longitude = request.GET.get('nlon', '')
+# def get(request):
+# 	sw_latitude = request.GET.get('slat', '')
+# 	sw_longitude = request.GET.get('slon', '')
+# 	ne_latitude = request.GET.get('nlat', '')
+# 	ne_longitude = request.GET.get('nlon', '')
 
-	
+# 	
