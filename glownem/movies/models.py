c6350bd133db1f896e78f261745a7619241ed72a
Josh Beal <jbeal24@gmail.com>
2012-10-12 21:20:40 -0700

    models

--- glownem/movies/models.py
+++ glownem/movies/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Movie(models.Model):
+    name = models.CharField(max_length=128)
+    year = models.IntegerField()
+
+class Location(models.Model):
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+    movie = models.ForeignKey(Movie)
+    
